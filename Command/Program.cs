d693eb6a2c2d8e0fa9dc2b222aee0781f84d5e6e
Rəsul <71267982+ResuIl@users.noreply.github.com>
2022-11-28 01:49:23 +0400

    Add project files.

---
+++ Command/Program.cs
@@ -0,0 +1,203 @@
+﻿using ArrayToPdf;
+using ClosedXML.Excel;
+using System.Data;
+using System.IO.Compression;
+
+#nullable disable
+// Nuget: ClosedXML
+// Nuget: ArrayToPdf
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+}
+
+// Receiver
+public class ExcelFile<T>
+{
+    private readonly List<T> _list;
+    public string FileName => $"{typeof(T).Name}.xlsx";
+
+
+    public ExcelFile(List<T> list)
+    {
+        _list = list;
+    }
+
+    public MemoryStream Create()
+    {
+        var wb = new XLWorkbook();
+        var ds = new DataSet();
+
+        ds.Tables.Add(GetTable());
+        wb.Worksheets.Add(ds);
+
+        var excelMemory = new MemoryStream();
+        wb.SaveAs(excelMemory);
+
+        return excelMemory;
+    }
+
+
+    private DataTable GetTable()
+    {
+        var table = new DataTable();
+
+        var type = typeof(T);
+
+        type.GetProperties()
+            .ToList()
+            .ForEach(x => table.Columns.Add(x.Name, x.PropertyType));
+
+
+        _list.ForEach(x =>
+        {
+            var values = type.GetProperties()
+                                .Select(properyInfo => properyInfo
+                                .GetValue(x, null))
+                                .ToArray();
+
+            table.Rows.Add(values);
+        });
+
+        return table;
+    }
+}
+
+public class PdfFile<T>
+{
+    private readonly List<T> _list;
+    public string FileName => $"{typeof(T).Name}.pdf";
+
+
+    public PdfFile(List<T> list)
+    {
+        _list = list;
+    }
+
+    public MemoryStream Create()
+    {
+        return GetTable().ToPdfStream();
+    }
+
+
+    private DataTable GetTable()
+    {
+        var table = new DataTable();
+
+        var type = typeof(T);
+
+        type.GetProperties()
+            .ToList()
+            .ForEach(x => table.Columns.Add(x.Name, x.PropertyType));
+
+
+        _list.ForEach(x =>
+        {
+            var values = type.GetProperties()
+                                .Select(properyInfo => properyInfo
+                                .GetValue(x, null))
+                                .ToArray();
+
+            table.Rows.Add(values);
+        });
+
+        return table;
+    }
+
+
+}
+
+public interface ITableActionCommand
+{
+    void Execute();
+}
+
+
+public class CreateExcelTableActionCommand<T> : ITableActionCommand
+{
+    private readonly ExcelFile<T> _excelFile;
+
+    public CreateExcelTableActionCommand(ExcelFile<T> excelFile)
+        => _excelFile = excelFile;
+
+    public void Execute()
+    {
+        MemoryStream excelMemoryStream = _excelFile.Create();
+        File.WriteAllBytes(_excelFile.FileName, excelMemoryStream.ToArray());
+    }
+}
+
+public class CreatePdfTableActionCommand<T> : ITableActionCommand
+{
+    private readonly PdfFile<T> _pdfFile;
+
+    public CreatePdfTableActionCommand(PdfFile<T> pdfFile)
+        => _pdfFile = pdfFile;
+
+    public void Execute()
+    {
+        MemoryStream excelMemoryStream = _pdfFile.Create();
+        File.WriteAllBytes(_pdfFile.FileName, excelMemoryStream.ToArray());
+    }
+}
+
+// Invoker
+class FileCreateInvoker
+{
+    private ITableActionCommand _tableActionCommand;
+    private List<ITableActionCommand> tableActionCommands = new List<ITableActionCommand>();
+
+    public void SetCommand(ITableActionCommand tableActionCommand)
+    {
+        _tableActionCommand = tableActionCommand;
+    }
+
+    public void AddCommand(ITableActionCommand tableActionCommand)
+    {
+        tableActionCommands.Add(tableActionCommand);
+    }
+
+    public void CreateFile()
+    {
+        _tableActionCommand.Execute();
+    }
+
+    public void CreateFiles()
+    {
+        throw new NotImplementedException();
+        // ZipArchive
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        var products = Enumerable.Range(1, 30).Select(index =>
+            new Product
+            {
+                Id = index,
+                Name = $"Product {index}",
+                Price = index + 100,
+                Stock = index
+            }
+        ).ToList();
+
+
+
+        PdfFile<Product> receiverPdf = new(products);
+        ExcelFile<Product> receiverExcel = new(products);
+
+
+        FileCreateInvoker invoker = new();
+        invoker.SetCommand(new CreatePdfTableActionCommand<Product>(receiverPdf));
+        invoker.CreateFile();
+        invoker.SetCommand(new CreateExcelTableActionCommand<Product>(receiverExcel));
+        invoker.CreateFile();
+    }
+}
---
+++ Itarator/Program.cs
@@ -0,0 +1,54 @@
+﻿interface IIterator
+{
+    bool HasNext();
+    string Next();
+    void Reset();
+}
+
+interface ICollection
+{
+    public IIterator CreateIterator();
+}
+
+class ResulCollection : ICollection
+{
+    List<string> list = new();
+    public void Add(string item) => list.Add(item);
+    public void Remove(string item) => list.Remove(item);
+    public void Clear() => list.Clear();
+    public IIterator CreateIterator() => new ResulIterator(list);
+}
+
+class ResulIterator : IIterator
+{
+    List<string> List = new();
+    int pos = 0;
+    public ResulIterator(List<string> list) => List = list;
+    public bool HasNext() => List.Count > pos;
+    public string Next() => List[pos++];
+    public void Reset() => pos = 0;
+}
+
+class Program
+{
+    static void Main()
+    {
+        ResulCollection notification = new ResulCollection();
+        notification.Add("one");
+        notification.Add("two");
+        notification.Add("three");
+        IIterator iterator = notification.CreateIterator();
+
+        while(iterator.HasNext())
+        {
+            Console.WriteLine(iterator.Next());
+        }
+
+        iterator.Reset();
+
+        while (iterator.HasNext())
+        {
+            Console.WriteLine(iterator.Next());
+        }
+    }
+}
